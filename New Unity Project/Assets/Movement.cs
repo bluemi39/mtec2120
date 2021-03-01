a3deafc696dccee4dacfbae8401a1366a6a351cf
bluemi39 <47123010+bluemi39@users.noreply.github.com>
2021-03-01 13:52:01 -0500

    Camera now moves with WASD keys
    To move camera with WASD make sure to be in play mode, while in the Unity editor.

---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Transform MainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = GetComponent<Transform>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, 0f, 10f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(10f * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-10f * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, 0f, -10f * Time.deltaTime);
+        }
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
